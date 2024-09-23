8154d77b7beb3fe8f68f698819033f528300c2d7
hungnnhe161718 <hungnnhe161718@fpt.edu.vn>
2024-09-23 21:36:38 +0700

    Category Api

---
+++ BE/BE/Controllers/Expert/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using BE.DTOs.ExpertDto;
+using BE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE.Controllers.Expert
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly OnlineLearningSystemContext _context;
+        public CategoryController(OnlineLearningSystemContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("ListAllCategory")]
+        public async Task<ActionResult<IEnumerable<Category>>> ListAllCategory()
+        {
+            var categories = await _context.Categories
+                .Select(l => new CategoryDto
+                {
+                    Id = l.Id,
+                    Name = l.Name,
+                    Type = l.Type,
+                })
+                .ToListAsync();
+            return Ok(categories);
+        }
+    }
+}
