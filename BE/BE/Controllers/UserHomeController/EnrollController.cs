dca4b92f925bb825045027a649aec255c2cb2f74
anhndHE160131 <anhndhe160131@fpt.edu.vn>
2024-10-16 17:51:42 +0700

    Update EnrollController.cs

--- BE/BE/Controllers/UserHomeController/EnrollController.cs
+++ BE/BE/Controllers/UserHomeController/EnrollController.cs
@@ -20,41 +20,53 @@ namespace BE.Controllers.UserHomeController
         {
             try
             {
-                var reg =  _learningSystemContext.Registrations.Where(x => x.UserId == accid).FirstOrDefault(x => x.SubjectId == subid);
+                var reg = _learningSystemContext.Registrations.Where(x => x.UserId == accid).FirstOrDefault(x => x.SubjectId == subid);
                 if (reg == null || reg.Status == "Pending")
                 {
                     var subprice = _learningSystemContext.PricePackages.FirstOrDefault(x => x.SubjectId == subid);
                     return Ok(subprice.SalePrice);
 
                 }
-             
+
                 return Ok("Bạn đã đăng ký môn học này");
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
-        
+
         [HttpPost]
         public async Task<IActionResult> RegisterSubject(int accid, int subid)
         {
             try
             {
                 var pp = await _learningSystemContext.PricePackages.Include(x => x.Subject).FirstOrDefaultAsync(x => x.SubjectId == subid);
-                var reg = new Registration
+                var reg = _learningSystemContext.Registrations.Where(x => x.UserId == accid).FirstOrDefault(x => x.SubjectId == subid);
+                if (reg == null)
                 {
-                    UserId = accid,
-                    SubjectId = subid,
-                    PackageId = pp.Id,
-                    RegistrationTime = DateTime.Now,
-                    TotalCost = pp.SalePrice,
-                    Status = "Pending",
-                    ValidFrom = DateTime.Now,
-                    ValidTo = DateTime.Now.AddDays(pp.DurationMonths)
-                };
-                await _learningSystemContext.Registrations.AddAsync(reg);
-                await _learningSystemContext.SaveChangesAsync();
-                return Ok(reg);
+                    var reg1 = new Registration
+                    {
+                        UserId = accid,
+                        SubjectId = subid,
+                        PackageId = pp.Id,
+                        RegistrationTime = DateTime.Now,
+                        TotalCost = pp.SalePrice,
+                        Status = "Pending",
+                        ValidFrom = DateTime.Now,
+                        ValidTo = DateTime.Now.AddMonths(pp.DurationMonths)
+                    };
+                    await _learningSystemContext.Registrations.AddAsync(reg);
+                    await _learningSystemContext.SaveChangesAsync();
+                    return Ok(reg1);
+                }
+                else if (reg != null && reg.Status == "Pending")
+                {
+                    return Ok(reg);
+                }
+                else { return BadRequest(); }
+
+
             }
             catch (Exception ex)
             {
@@ -71,7 +83,8 @@ namespace BE.Controllers.UserHomeController
                 _learningSystemContext.Registrations.Update(reg);
                 await _learningSystemContext.SaveChangesAsync();
                 return Ok(reg);
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
