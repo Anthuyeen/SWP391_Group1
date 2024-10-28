73411be4878f19450c327e5761954276c7b61a54
hungnnhe161718 <hungnnhe161718@fpt.edu.vn>
2024-10-28 18:09:37 +0700

    Update AddQuestion

--- front-end/src/pages/expert/component/add-question.js
+++ front-end/src/pages/expert/component/add-question.js
@@ -10,7 +10,7 @@ const AddQuestion = () => {
     const [newQuestion, setNewQuestion] = useState({
         content: '',
         mediaUrl: '',
-        status: 'Active', // Mặc định trạng thái là Active
+        status: 'DeActive', // Mặc định trạng thái là DeActive
         answers: [{ content: '', isCorrect: false }],
     });
 
