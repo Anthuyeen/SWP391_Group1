1df6b0d9fccc19a76f5b069980fab42063b1ecb1
hungnnhe161718 <hungnnhe161718@fpt.edu.vn>
2024-11-08 00:17:21 +0700

    update fetchRegister

--- front-end/src/service/authAPI.js
+++ front-end/src/service/authAPI.js
@@ -34,22 +34,7 @@ export const fetchLogin = async (email, password) => {
 //     return response.json();
 // };
 
-export const fetchRegister = async (userData) => {
-    const response = await fetch('https://localhost:7043/api/Authentication/Register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-    });
 
-    if (!response.ok) {
-        const errorText = await response.text(); // Lấy thông báo lỗi từ response
-        throw new Error(errorText); // Ném lỗi với thông báo chi tiết
-    }
-
-    return response.json();
-};
 
 //send otp to mail
 export const sendOtpEmail = async (email) => {
